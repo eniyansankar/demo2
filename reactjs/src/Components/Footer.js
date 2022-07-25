226365728461e2866e32d521299310c57975ed97
eniyansankar <eniyanhst@gmail.com>
2022-07-25 15:51:37 +0530

    init

--- reactjs/src/Components/Footer.js
+++ reactjs/src/Components/Footer.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import './Style.css';
+import Container from '@mui/material/Container'
 
 function Footer() {
     return (
-        <div>
-            <footer>
-           <p>rediff@gmail.com</p>
-           </footer>
+        <>
+        <div className='main-footer'>
+         <div className='container'>
+         <div className='row'>
+         <div className='col-md-3 col-sm-6'>
+            <h3>
+                eweqwewqewqewe
+            </h3>
         </div>
+            </div>
+
+         </div>
+        </div>
+        
+        
+        
+        
+        
+        </>
+        
     );
 }
 
