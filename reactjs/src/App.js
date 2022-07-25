998c20a59c240e4ad114eb5d303e874cb9ed6a30
eniyansankar <eniyanhst@gmail.com>
2022-07-25 15:51:41 +0530

    init

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,29 +1,140 @@
 import React from 'react';
-import Footer from './Components/Footer';
-
-import Header from './Components/Header';
-import ParaSec from './Components/ParaSec';
-import Slider from './Components/Slider';
-import TBoxes from './Components/TBoxes';
-
-
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Col, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Figure from 'react-bootstrap/Figure';
 
 function App() {
-  return (
-    <div>
-   
-   
-  <Header/>
-  <Slider/>
-  <ParaSec/>
-  <div style={{clear:'both'}}></div>
-  <TBoxes/>
-  <div style={{clear:'both'}}></div>
-  <Footer/>
+    return (
+        <Container>
+        <Navbar bg="warning" expand="lg">
+        
+          <Navbar.Brand href="#home"><h3>Finacale.Com</h3></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        
+      </Navbar>
+     
+      <Row>
+      <Col>
+      <Figure>
+      <Figure.Image
+        width={800}
+        height={800}
+        alt="171x180"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnPKr9gqctSdbY3sX8PDmSmW2tpnmqmWmi-Q&usqp=CAU"
+      />
+      <Figure.Caption>
+        Nulla vitae elit libero, a pharetra augue mollis interdum.
+      </Figure.Caption>
+    </Figure>
+      
+        </Col>
+       
+       
+        <Col>
+        <Card style={{ width: '25rem'} }>
+      <Card.Img variant="top" src="https://images.pexels.com/photos/730564/pexels-photo-730564.jpeg?auto=compress&cs=tinysrgb&w=600" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+       </Col>
+      </Row>
+      <Container fluid="md bg-info">
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+    <Row>
+    <Col>
+        <Card style={{ width: '15rem'} }>
+      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd_z_gw_5A_jgGI4s0aUxo-z2O1qjx2a3eNA&usqp=CAU" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+       </Col>
+       <Col>
+        <Card style={{ width: '15rem'} }>
+      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYrVHFcFDuIlecJwdPHzoRd0vxJjkSGodnKg&usqp=CAU" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+       </Col>
+       <Col>
+        <Card style={{ width: '15rem'} }>
+      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsMYVC0x5MsVCcHM-euKXunm7Qvsscq512uQ&usqp=CAU" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+       </Col>
+       <Container fluid="md bg-warning">
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
     
-    </div>
-  );  
+     </Row>
+     <Row>
+     
+
+     <Container fluid="md bg-info ">
+      <Row>
+        <Col> rediff@gmail.co,</Col>
+      </Row>
+    </Container>
+
+
+    
+     </Row>
+      </Container>
+      
+        
+    );
 }
 
 export default App;
